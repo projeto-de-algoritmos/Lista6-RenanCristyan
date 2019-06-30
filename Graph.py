693916f83d8bf52652a118fcf93274a17d1ff624
RCristyan <rcristyan9@gmail.com>
2019-06-30 19:26:05 -0300

    Adicionada função para salvar um grafo em .png

--- Graph.py
+++ Graph.py
@@ -3,6 +3,9 @@
 import math
 from queue import PriorityQueue
 from random import randint
+import networkx as nx
+import statistics
+import matplotlib.pyplot as plt
 
 # Implementacao de Grafo baseada em http://www.python-course.eu/graphs_python.php
 class Graph(object):
@@ -162,6 +165,41 @@ def generate_random_graph(nodes, max_cost=20):
 
     return _g
 
+# Implementacao baseada no exemplo disponivel em:
+# https://networkx.readthedocs.io/en/stable/examples/drawing/weighted_graph.html
+def save_graph_as_png(graph, name=False):
+    print('\n')
+    nxg = nx.Graph()
+    costs = []
+    for (a, b, cost) in graph.edges():
+        nxg.add_edge(a, b, cost=cost)
+        costs.append(cost)
+
+    pos = nx.spring_layout(nxg)  # positions for all nodes
+
+    avg_cost = statistics.mean(costs)
+
+    elarge = [(u, v) for (u, v, d) in nxg.edges(data=True) if d['cost'] > avg_cost]
+    esmall = [(u, v) for (u, v, d) in nxg.edges(data=True) if d['cost'] <= avg_cost]
+
+    # nodes
+    nx.draw_networkx_nodes(nxg, pos, node_size=700)
+
+    # edges
+    nx.draw_networkx_edges(nxg, pos, edgelist=elarge, width=4)
+    nx.draw_networkx_edges(nxg, pos, edgelist=esmall, width=4, alpha=0.5, edge_color='b', style='dashed')
+
+    # labels
+    nx.draw_networkx_labels(nxg, pos, font_size=20, font_family='sans-serif')
+    nx.draw_networkx_edge_labels(nxg, pos)
+
+    plt.axis('off')
+    
+    if name:
+        plt.savefig("{}.png".format(name))
+    else:
+        plt.savefig("graph.png")  # save as png
+
 if __name__ == '__main__':
 
     g = Graph({})
@@ -179,8 +217,12 @@ if __name__ == '__main__':
     g.show_vertices()
     g.show_edges()
     print('--')
+
     print_dijkstra(g)
     print('--')
+    
     print('Minimal Spanning Tree (Peso Final = %s):' % w)
     g_prim.show_vertices()
     g_prim.show_edges()
+
+    save_graph_as_png(g_prim, 'arvore_geradora_minima')
