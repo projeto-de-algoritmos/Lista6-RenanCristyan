3c0e2cf0e047c672f466227fbff167ef9a3d63d3
RCristyan <rcristyan9@gmail.com>
2019-07-01 09:55:20 -0300

    Adicionada função para salvar o grafo em .png

--- Kruskal.py
+++ Kruskal.py
@@ -1,5 +1,9 @@
 # Código retirado de: https://gist.github.com/hayderimran7/09960ca438a65a9bd10d0254b792f48f
 
+import networkx as nx
+import statistics
+import matplotlib.pyplot as plt
+
 parent = dict()
 rank = dict()
 
@@ -42,10 +46,13 @@ def kruskal(graph):
 
 def print_kruskal(graph):
 	k = kruskal(graph)
+
 	print('Resultado do algoritmo de kruskal:')
 
 	for node in k:
 		print(node)
+
+	save_graph_as_png(k, 'kruskal')
 
 def print_graph(graph):
 	print('Grafo original:')
@@ -58,6 +65,43 @@ def print_graph(graph):
 	print('Edges: ')
 	for e in graph['edges']:
 		print(e)
+
+	# save_graph_as_png(graph, 'kruskal')
+
+def save_graph_as_png(graph, name=False):
+    print('\n')
+    nxg = nx.Graph()
+    costs = []
+
+    for (cost, a, b) in graph['edges']:
+        nxg.add_edge(a, b, cost=cost)
+        costs.append(cost)
+
+    pos = nx.spring_layout(nxg)  # positions for all nodes
+
+    avg_cost = statistics.mean(costs)
+
+    elarge = [(u, v) for (u, v, d) in nxg.edges(data=True) if d['cost'] > avg_cost]
+    esmall = [(u, v) for (u, v, d) in nxg.edges(data=True) if d['cost'] <= avg_cost]
+
+    # nodes
+    nx.draw_networkx_nodes(nxg, pos, node_size=700)
+
+    # edges
+    nx.draw_networkx_edges(nxg, pos, edgelist=elarge, width=4)
+    nx.draw_networkx_edges(nxg, pos, edgelist=esmall, width=4, alpha=0.5, edge_color='b', style='dashed')
+
+    # labels
+    nx.draw_networkx_labels(nxg, pos, font_size=20, font_family='sans-serif')
+    nx.draw_networkx_edge_labels(nxg, pos)
+
+    plt.axis('off')
+    
+    if name:
+        plt.savefig("{}.png".format(name))
+    else:
+        plt.savefig("graph.png")  # save as png
+
 
 
 graph = {
